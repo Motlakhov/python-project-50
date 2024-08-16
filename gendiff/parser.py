4a09f51e45681b38e57dbb42cc363a4a3375eba4
Motlakhov <dmitrijmotlahov@gmail.com>
2024-08-16 19:19:48 +0300

    step 5

---
+++ gendiff/parser.py
@@ -0,0 +1,10 @@
+import yaml
+import json
+
+def parse_file(filepath):
+	if filepath.endswith == 'json':
+		return file1 = json.load(open(file_path1))
+  		file2 = json.load(open(file_path2))
+	if filepath.endswith == 'yml', 'yaml':
+		return file1 = yaml.safe_load(open(file_path1))
+                file2 = yaml.safe_load(open(file_path2))
--- tests/test_files.py
+++ tests/test_files.py
@@ -6,6 +6,11 @@ from gendiff.scripts.generate_diff import generate_diff
         "tests/fixtures/file1.json",  # Путь к первому файлу
         "tests/fixtures/file2.json",  # Путь ко второму файлу
         '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true'
+    ),
+    (
+        "tests/fixtures/file1.yml",  # Путь к первому файлу
+        "tests/fixtures/file2.yml",  # Путь ко второму файлу
+        '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true'
     )
 ])
 
