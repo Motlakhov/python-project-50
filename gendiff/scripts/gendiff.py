95cef2c2941ad247aa57c3686d738176544cd995
Motlakhov <dmitrijmotlahov@gmail.com>
2024-08-31 18:30:29 +0300

    lint

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,8 +5,7 @@ from gendiff.scripts.generate_diff import generate_diff
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-        )
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
     parser.add_argument('-f', '--format',
--- tests/test_files.py
+++ tests/test_files.py
@@ -1,6 +1,7 @@
 import pytest
 from gendiff.scripts.generate_diff import generate_diff
 
+
 @pytest.mark.parametrize("file1, file2, formatter, expected_output", [
     (
         "tests/fixtures/file1.json",  # Путь к первому файлу
@@ -9,37 +10,36 @@ from gendiff.scripts.generate_diff import generate_diff
         "tests/fixtures/expected_output_stylish.txt"
     ),
     (
-        "tests/fixtures/file1.yml",  
-        "tests/fixtures/file2.yml",  
+        "tests/fixtures/file1.yml",
+        "tests/fixtures/file2.yml",
         "stylish",
         "tests/fixtures/expected_output_stylish.txt"
     ),
     (
-        "tests/fixtures/file1.json",  
-        "tests/fixtures/file2.json",  
+        "tests/fixtures/file1.json",
+        "tests/fixtures/file2.json",
         "plain",
         "tests/fixtures/expected_output_plain.txt"
     ),
     (
-        "tests/fixtures/file1.yml", 
-        "tests/fixtures/file2.yml",  
+        "tests/fixtures/file1.yml",
+        "tests/fixtures/file2.yml",
         "plain",
         "tests/fixtures/expected_output_plain.txt"
     ),
     (
-        "tests/fixtures/file1.json",  
-        "tests/fixtures/file2.json",  
+        "tests/fixtures/file1.json",
+        "tests/fixtures/file2.json",
         "json",
         "tests/fixtures/expected_output_json.txt"
     ),
     (
-        "tests/fixtures/file1.yml",  
-        "tests/fixtures/file2.yml",  
+        "tests/fixtures/file1.yml",
+        "tests/fixtures/file2.yml",
         "json",
         "tests/fixtures/expected_output_json.txt"
     )
 ])
-
 def test_generate_diff(file1, file2, formatter, expected_output):
     diff = generate_diff(file1, file2, formatter)
     expected_result = read_file(expected_output)
@@ -49,3 +49,4 @@ def test_generate_diff(file1, file2, formatter, expected_output):
 def read_file(file_name):
     with open(file_name, 'r') as file:
         return file.read().strip()
+    
